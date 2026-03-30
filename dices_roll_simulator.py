0bfc34bbabbe26ef68fd11b30e689e1de27a56e3
Poornimashree <156743539+Poornimashree@users.noreply.github.com>
2026-03-30 17:39:02 +0530

    Delete dices_roll_simulator.py

--- dices_roll_simulator.py
+++
@@ -1,9 +0,0 @@
-import random
-
-while True:
-    input("Press Enter to roll dice...")
-    print("Dice:", random.randint(1, 6))
-
-    choice = input("Roll again? (y/n): ")
-    if choice != 'y':
-        break
